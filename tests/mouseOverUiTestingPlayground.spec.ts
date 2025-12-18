36ee991a1182786279347b6814ad676bc4cc0e2b
lpascuar2m <lpascua.r2m@gmail.com>
2025-12-18 10:48:39 +0800

    change to use google chrome and add base url to the project

--- tests/mouseOverUiTestingPlayground.spec.ts
+++ tests/mouseOverUiTestingPlayground.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('http://uitestingplayground.com/mouseover');
+    await page.goto('/mouseover');
 });
 
 test.afterEach(async ({ page }) => {
