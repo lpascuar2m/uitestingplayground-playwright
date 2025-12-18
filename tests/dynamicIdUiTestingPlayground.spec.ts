5795a47b02f91bfe8d0500a90810041e66bee941
lpascuar2m <lpascua.r2m@gmail.com>
2025-12-18 10:53:36 +0800

    add dynamic id test

---
+++ tests/dynamicIdUiTestingPlayground.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/dynamicid');
+});
+
+test.afterEach(async ({ page }) => {
+    await page.close();
+});
+
+test("Record 2 consecutive link clicks.", async ({ page }) => {
+    const button = page.getByRole('button', { name: 'Button with Dynamic ID' });
+    await expect(await button).toBeVisible();
+    await expect(await button).toBeEnabled();
+    await button.click();
+});
